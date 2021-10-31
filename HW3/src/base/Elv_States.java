fd4026690d2a05168f55e382f5b9d2de734739d2
engelsm <82509769+engelsm@users.noreply.github.com>
2021-10-31 10:59:50 +0100

    Add files via upload
    solve HW3

---
+++ HW3/src/base/Elv_States.java
@@ -0,0 +1,14 @@
+package base;
+
+public class Elv_States {
+
+	public String Moving_up = "Moving_up";
+	public String Moving_down = "Moving_down";
+	public String idle= "idle";
+	public String current_State;
+	
+	public Elv_States() {
+		current_State = idle;
+	}
+
+}
